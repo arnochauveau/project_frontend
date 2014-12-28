ee2479bfb5f3e0b20463394cce522eb95aa00a68
arno chauveau <arnochauveau@gmail.com>
2014-12-28 14:59:24 +0100

    added mongodb functionality to save chat messages

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var mongoose = require('mongoose');
+
+
 var nicknames = [];
 
 //ROUTING
@@ -24,6 +27,12 @@ app.get('/chat',function(req,res){
 
 //socket stuff
 io.sockets.on("connection", function (socket) {
+
+    ChatModel.find({}).sort('-created').limit(8).exec(function(err,docs){
+        if(err) console.log(err);
+        socket.emit('load old msgs',docs);
+    });
+
     // here are connections from /new
     socket.on('new user',function(data,callback){
         console.log("new user");
@@ -50,7 +59,13 @@ io.sockets.on("connection", function (socket) {
     });
 
     socket.on('send message',function(data){
-        io.sockets.emit('new message',{msg:data,nick:socket.nickname});
+        var newMsg = new ChatModel({msg: data,nick: socket.nickname});
+        newMsg.save(function(err){
+           if(err) console.log(err);
+
+            io.sockets.emit('new message',{msg:data,nick:socket.nickname});
+
+        });
     });
 
 
@@ -66,4 +81,18 @@ io.sockets.on("connection", function (socket) {
     console.log('Webserver listening at http://%s:%s',host,port);
 
 });
+
+mongoose.connect('mongodb://localhost/chat',function(err){
+   if(err){
+       console.log(err);
+   }
+});
+
+var chatSchema = mongoose.Schema({
+    nick: String,
+    msg: String,
+    created: {type:Date, default: Date.now()}
+});
+
+var ChatModel = mongoose.model('Message',chatSchema);
 
